bc75eebddbf880af851f3674f9064f3b697a3bbc
David <dvelezmarulanda@gmail.com>
2026-04-21 12:21:03 -0500

    Creando archivo de configuracion

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/react-swc'
 
 export default defineConfig({
-  base: '/proyectos-antigravity/',
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+    plugins: [react()],
+    base: './',
 })
